50c42729b4db1f11c7b923a9dfb762fce8eb1cca
Bryan <bryanrivera21403@gmail.com>
2020-08-10 17:04:56 -0400

    More css and homePage photo

--- script.js
+++ script.js
@@ -1,14 +1,30 @@
 let url = "https://api.diffbot.com/v3/product"; 
-//API taken from https://www.diffbot.com/dev/docs/product/
-//called a "product API" that "extracts complete data from any shopping or e-commerce product page"
-
+// //API taken from https://www.diffbot.com/dev/docs/product/
+// //called a "product API" that "extracts complete data from any shopping or e-commerce product page"
+// // token is e2d949e1283ff9c25c322abc9c2f47a1
 fetch(url)
-    .then(function(response){
-        return response.json();
-    })
-    .then(function(myJson){
-        console.log(myJson);    
-        // let email = (myJson.results[0].email);
-        // console.log(email);
-        // displayData(myJson.results[0]);
-    })
+    .then(response => 
+        response.json()
+        )
+    .then(json => 
+        console.log(json)
+        );
+
+
+//called when successful user log in
+function onSignIn(googleUser) {
+    let profile = googleUser.getBasicProfile();
+
+    console.log('User signed in!');
+    console.log(profile.getName());
+    console.log(profile.getImageUrl());
+    console.log(profile.getEmail());
+}
+
+//called when "sign out" button clicked
+function onSignOut() {
+    let auth2 = gapi.auth2.getAuthInstance();
+    auth2.signOut().then(function () {
+        console.log('User signed out.');
+    });
+}
